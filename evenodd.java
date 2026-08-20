7df138fb48f8aaadd08c74ca5ea335579d648040
Haidar Raj Ali <haidar.25scs1003004846@iilm.edu>
2026-08-20 21:48:08 +0530

    Create evenodd.java

---
+++ evenodd.java
@@ -0,0 +1,37 @@
+//Check Even/Odd and Positive/Negative
+
+
+import java.util.Scanner;
+
+public class evenodd {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+
+        if (num > 0) {
+            System.out.println("The number is Positive.");
+
+            if (num % 2 == 0) {
+                System.out.println("The number is Even.");
+            } else {
+                System.out.println("The number is Odd.");
+            }
+
+        } else if (num < 0) {
+            System.out.println("The number is Negative.");
+
+            if (num % 2 == 0) {
+                System.out.println("The number is Even.");
+            } else {
+                System.out.println("The number is Odd.");
+            }
+
+        } else {
+            System.out.println("The number is Zero.");
+        }
+
+        sc.close();
+    }
+}
